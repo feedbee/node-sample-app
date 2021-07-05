90fa3c2a9c505edf62624bd82d5b6c2f9076f66f
Valery Leontyev <feedbee@gmail.com>
2021-07-05 20:42:40 +0000

    Various features to modify the behaviour and output

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ const http = require('http');
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+let counter = 0;
+let state = "";
+const server = http.createServer(async (req, res) => {
+  counter++;
+  
+  const url = new URL(req.url, `http://${req.headers.host}/`);
+  let slept = 0;
+  if (url.searchParams.has('sleep')) {
+    slept = parseInt(url.searchParams.get('sleep'));
+    await (ms => new Promise(resolve => setTimeout(resolve, ms)))(slept);
+  }
+
+  let retCode = 200;
+  if (url.searchParams.has('code')) {
+    retCode = parseInt(url.searchParams.get('code'));
+  }
+
+  res.statusCode = retCode;
   res.setHeader('Content-Type', 'text/plain');
+
+  if (url.searchParams.has('noise')) {
+    res.write(require("crypto").randomBytes(parseInt(url.searchParams.get('noise'))).toString('base64'));
+    res.end();
+    return;
+  }
+
   res.write("Node.JS Sample App\n");
   res.write(`Remote Address:Port: ${req.socket.remoteAddress}:${req.socket.remotePort}\n`);
   res.write(`Local Address:Port: ${req.socket.localAddress}:${req.socket.localPort}\n`);
@@ -37,7 +60,25 @@ const server = http.createServer((req, res) => {
     for (let name in process.env) {
       envs.push(`\t${name}: ${process.env[name]}`);
     }
-    res.write(`ENV variables (${process.env / 2}):\n${envs.join("\n")}\n`);
+    res.write(`ENV variables (${process.env.length / 2}):\n${envs.join("\n")}\n`);
+
+    res.write(`\nRequest #${counter}\n`);
+    res.write(`Return HTTP code: ${retCode}\n`);
+    if (slept > 0) {
+      res.write(`Slept (response delay): ${slept} ms\n`);
+    }
+    if (state !== "" && state !== null) {
+      res.write(`Saved state: ${state}\n`);
+    }
+
+    if (url.searchParams.has('state')) {
+      state = url.searchParams.get('state');
+    }
+
+    res.write(`\nUsage: \n- ?sleep=N - response deley in ms (like 2000 = 2 seconds)\n`);
+    res.write(`- ?code=N - response HTTP code as a number (like 200 = OK)\n`);
+    res.write(`- ?state=S - save some string value between requests\n`);
+    res.write(`- ?noise=N - generate N bytes of random data and return it base64 encoded\n`);
 
     res.end();
   })  
