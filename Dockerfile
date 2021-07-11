9af93884b2ef494af91ebd7bd083c0b58b392f60
Valery Leontyev <feedbee@gmail.com>
2021-07-11 21:16:14 +0000

    Adding persistent storage with Redis

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ ENV HOST "0.0.0.0"
 ENV PORT 3000
 EXPOSE 3000
 
+COPY node_modules /usr/src/app/node_modules
 COPY server.js /usr/src/app/server.js
 WORKDIR /usr/src/app/
 
--- server.js
+++ server.js
@@ -2,11 +2,62 @@ const http = require('http');
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
+const PERSISTENT = !!process.env.PERSISTENT;
 
-let counter = 0;
-let state = "";
+class RedisStorage {
+  constructor(redisConfig) {
+    this.redis = require("redis");
+    const { promisifyAll } = require('bluebird');
+    promisifyAll(this.redis);
+
+    this.client = this.redis.createClient(redisConfig);
+    this.client.on("error", (error) => console.error(error));
+  }
+
+  async getCounter() {
+    return this.client.getAsync('node-sample-app-counter');
+  }
+  async increment() {
+    await this.client.incrAsync('node-sample-app-counter');
+  }
+
+  async getState() {
+    return this.client.getAsync('node-sample-app-state');
+  }
+  async setState(value) {
+    await this.client.setAsync('node-sample-app-state', value);
+  }
+}
+class LocalStorage {
+  constructor() {
+    this.counter = 0;
+    this.state = "";
+  }
+
+  getCounter() {
+    return this.counter;
+  }
+  increment() {
+    this.counter++;
+  }
+
+  getState() {
+    return this.value;
+  }
+  setState(value) {
+    this.value = value;
+  }
+}
+
+let storage;
+if (PERSISTENT) {
+  storage = new RedisStorage({host: 'redis'});
+} else {
+  storage = new LocalStorage();
+}
+
 const server = http.createServer(async (req, res) => {
-  counter++;
+  await storage.increment();
   
   const url = new URL(req.url, `http://${req.headers.host}/`);
   let slept = 0;
@@ -45,7 +96,7 @@ const server = http.createServer(async (req, res) => {
   req.on('data', chunk => {
     res.write(chunk);
   });
-  req.on('end', () => {
+  req.on('end', async () => {
     res.write(`\n---\nBytes Read: ${req.socket.bytesRead}\n\n`);
 
     var os = require("os");
@@ -60,19 +111,22 @@ const server = http.createServer(async (req, res) => {
     for (let name in process.env) {
       envs.push(`\t${name}: ${process.env[name]}`);
     }
-    res.write(`ENV variables (${process.env.length / 2}):\n${envs.join("\n")}\n`);
+    res.write(`ENV variables (${envs.length}):\n${envs.join("\n")}\n`);
 
+    let counter = await storage.getCounter();
     res.write(`\nRequest #${counter}\n`);
+    res.write(`\nPersistence: ${PERSISTENT}\n`);
     res.write(`Return HTTP code: ${retCode}\n`);
     if (slept > 0) {
       res.write(`Slept (response delay): ${slept} ms\n`);
     }
-    if (state !== "" && state !== null) {
+    let state = await storage.getState();
+    if (state !== "" && state !== null && state !== undefined) {
       res.write(`Saved state: ${state}\n`);
     }
 
     if (url.searchParams.has('state')) {
-      state = url.searchParams.get('state');
+      await storage.setState(url.searchParams.get('state'));
     }
 
     res.write(`\nUsage: \n- ?sleep=N - response deley in ms (like 2000 = 2 seconds)\n`);
